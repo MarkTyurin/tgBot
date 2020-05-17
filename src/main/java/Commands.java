771610f7676bac5dc5705b5122899e143077270b
Mark Tyurin <tyurin.markk@gmail.com>
2020-05-18 02:51:35 +0300

    i6

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -26,7 +26,7 @@ public class Commands {
 
             case "/hi": {
 
-                return " Вас /**/приветствует/**/ бот-помошник. ";
+                return " __Вас__ **приветствует** бот-помошник. ";
             }
             case "/coin":
                 return "И выпадает: " + (new Random().nextInt(2) == 0 ? "Орёл" : "Решка");
