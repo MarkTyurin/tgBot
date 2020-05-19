59d91f94bce8cbfbc75c9fc4020c0cf0438d7fe4
Mark Tyurin <tyurin.markk@gmail.com>
2020-05-19 21:34:54 +0300

    e5

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -196,13 +196,13 @@ public class Bot extends TelegramLongPollingBot {
                     List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
                     List<InlineKeyboardButton> rowInline = new ArrayList<>();
                     List<InlineKeyboardButton> rowInline2 = new ArrayList<>();
-                    rowsInline.add(rowInline);
+                  //  rowsInline.add(rowInline);
                     rowsInline.add(rowInline2);
                     // Add it to the message
                     markupInline.setKeyboard(rowsInline);
                     List<Genre> genres = run.query(Db.connecti, "SELECT * FROM Genre", h);
                     for (Genre genre : genres) {
-                        rowInline.add(new InlineKeyboardButton().setText(genre.getName()).setCallbackData("/find_genre," + genre.getName()));
+                     ///   rowInline.add(new InlineKeyboardButton().setText(genre.getName()).setCallbackData("/find_genre," + genre.getName()));
                         rowInline2.add(new InlineKeyboardButton().setText("***************"));
 
                     }
